f1e167954b329527011c5819c4794b93f5600443
JonCyprus <joncyprus99@gmail.com>
2024-12-15 18:47:37 -0800

    screen works

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
@@ -7,12 +11,55 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
+
+    # Create groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    # Containerize groups
+    Asteroid.containers = (updatable, drawable, asteroids)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    asteroid_field = AsteroidField()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+
+        # Update groups
+        for member in updatable:
+            member.update(dt)
+
+        # Collision check
+        for member in asteroids:
+            if member.colliding(player):
+                print("Game Over!")
+                pygame.quit()
+                return 0
+
+        for member in asteroids:
+            for bullet in shots:
+                if member.colliding(bullet):
+                    member.kill()
+                    dddd
+
+        # Draw on the screen
+        screen.fill("black")
+        for member in drawable:
+            member.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
